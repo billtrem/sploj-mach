141a8fd5a5f33104301548d4a139413311fc9062
billtrem <wsf.tremlett@gmail.com>
2025-04-21 19:28:02 +0100

    Add fallback-safe Wasabi key loading

--- splojsite/settings.py
+++ splojsite/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-from decouple import config
+from decouple import config, UndefinedValueError
 import dj_database_url
 
 # Base directory
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'storages',  # Added for Wasabi/S3 support
+    'storages',  # Wasabi/S3 support
 ]
 
 # Middleware
@@ -92,8 +92,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Wasabi S3)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+
+try:
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+except UndefinedValueError:
+    AWS_ACCESS_KEY_ID = 'dummy-key'
+
+try:
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+except UndefinedValueError:
+    AWS_SECRET_ACCESS_KEY = 'dummy-secret'
+
 AWS_STORAGE_BUCKET_NAME = 'sploj-media'
 AWS_S3_ENDPOINT_URL = 'https://s3.eu-west-1.wasabisys.com'
 AWS_S3_REGION_NAME = 'eu-west-1'
