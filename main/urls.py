f3576579aa7fbdcb8ecb3ae6f2542bbe5c519ea4
billtrem <wsf.tremlett@gmail.com>
2025-09-06 22:12:05 +0100

    Redirect homepage to /whats-on/

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path("whats-on/", views.whats_on, name="whats_on"),
-    path("project-modal/<slug:slug>/", views.project_modal, name="project_modal"),
-    path("info/", views.info, name="info"),
+    # Redirect the homepage to /whats-on/
+    path('', RedirectView.as_view(url='/whats-on/', permanent=False)),
+
+    path('whats-on/', views.whats_on, name='whats_on'),
+    path('project-modal/<slug:slug>/', views.project_modal, name='project_modal'),
+    path('info/', views.info, name='info'),
 ]
